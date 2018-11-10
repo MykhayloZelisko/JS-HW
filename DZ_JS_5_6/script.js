866afd95c00468805e45cf5b13e03685632b872e
MykhayloZelisko <zelisko.mykhaylo@gmail.com>
2018-11-10 17:37:05 +0200

    no message
    Signed-off-by: MykhayloZelisko <zelisko.mykhaylo@gmail.com>

---
+++ DZ_JS_5_6/script.js
@@ -0,0 +1,36 @@
+﻿// Завдання 1
+
+//function factorial(n) {
+//    if (n > 1) {
+//        return n * factorial(n - 1)
+//    } else if (n === 1 || n === 0)
+//        return 1;
+//}
+//alert( factorial(10) ); 
+
+// Завдання 2
+
+//function fib(n) {
+//    if (n > 2) {
+//        return fib(n - 1) + fib(n - 2);
+//    } else if (n === 1 || n === 2) {
+//        return 1;
+//    }
+//}
+//alert( fib(10) ); 
+
+// Завдання 3
+
+var a = +prompt("a?", "");
+switch(a) {
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert("2,3")
+        break;
+}
